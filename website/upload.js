5339f6449cde1a65803f81bdc2c8cd0f58e48666
FatimaShibli <95264693+FatimaShibli@users.noreply.github.com>
2022-10-13 12:42:43 -0500

    Update upload.js
    meow

--- website/upload.js
+++ website/upload.js
@@ -1,1 +1,30 @@
 //for Fatima meow
+<form enctype="multipart/form-data">
+<input id="upload" type=file   accept="text/html" name="files[]" size=30>
+</form>
+
+<textarea class="form-control" rows=35 cols=120 id="ms_word_filtered_html"></textarea>
+
+<script>
+function handleFileSelect(evt) {
+    let files = evt.target.files; // FileList object
+
+    // use the 1st file from the list
+    let f = files[0];
+    
+    let reader = new FileReader();
+
+    // Closure to capture the file information.
+    reader.onload = (function(theFile) {
+        return function(e) {
+          
+          jQuery( '#ms_word_filtered_html' ).val( e.target.result );
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsText(f);
+  }
+
+  document.getElementById('upload').addEventListener('change', handleFileSelect, false);
+</script>
